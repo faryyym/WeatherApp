761cac56b8d0aeb66a1efeedabd49ad6eb6cffa7
faryyym <56434035+faryyym@users.noreply.github.com>
2020-08-12 16:09:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ class Weather {
     }
 
     async getWeather() {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`)
 
         const data = await response.json()
         return data;
